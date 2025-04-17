582c7ac1872e487a2fb7db38e2c89bfb87bc044f
Abdul Rehman <abdulrehmansafdarlodhi@gmail.com>
2025-04-17 16:10:17 +0500

    Basic Complete now before New year app

--- Week3_Django/lecture3/hi/views.py
+++ Week3_Django/lecture3/hi/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hey")
+    return render(request, "hi/index.html")
 
 def brain(request):
     return HttpResponse("Hey brain")
@@ -13,5 +13,7 @@ def david(request):
     return HttpResponse("Hey david")
 
 def greet(request, name):
-    return HttpResponse(f"As-Salaam-Alaikum {name.capitalize()}!")
+    return render(request, "hi/greet.html",{
+        "name": name.capitalize()
+    })
 
